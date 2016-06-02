2871568abe59f2b6678de2fa12eb86b708c8e6d0
Yasunari Flores <carlosyasu91@gmail.com>
2016-06-02 12:13:13 -0500

    Modified spritesheet, modified desert_tiles

--- client/state/Level1.js
+++ client/state/Level1.js
@@ -1,26 +1,32 @@
 BasicGame.Level1 = function (game) {
   this.player = null;
-  this.playerSpeed = 5;
+  this.playerSpeed = 3.5;
   this.playerMoving = false;
   this.map = null;
   this.layer = null;
   this.movementDirection = null;
-  this.keyPressed = null;
+  this.standingFPS = 0.75;
+  this.walkingFPS = 6;
+
+  this.movingLeft;
+  this.movingRight;
+  this.movingDown;
+  this.movingUp;
+
 };
 
 BasicGame.Level1.prototype = {
-
   init: function () {
     this.input.maxPointers = 1;
     this.stage.disableVisibilityChange = true;
     this.scale.pageAlignHorizontally = true;
   },
-
-  preload: function () {    this.load.spritesheet('player', 'images/basicChar.png', 64, 64, 19);
+  preload: function () {    
+    this.load.spritesheet('player', 'images/basicChar.png', 128, 128, 41);
+    this.load.spritesheet('sword', 'images/basicSword.png', 128, 128, 41);
     this.load.tilemap('desert', 'assets/tilemaps/desert.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('tiles', 'images/desert_tiles.png');
   },
-
   create: function () {
     this.map = this.add.tilemap('desert');
     this.map.addTilesetImage('Desert', 'tiles');
@@ -28,37 +34,114 @@ BasicGame.Level1.prototype = {
     layer.resizeWorld();
 
     this.player = this.add.sprite(300, 200, 'player');
-    this.player.anchor.setTo(.5, 1);
+    this.sword = this.add.sprite(0, 0, 'sword');
+    this.player.addChild(this.sword);
 
-    this.walkDown = this.player.animations.add('walkDown', [4,5,6,7]);
-    this.walkUp = this.player.animations.add('walkUp', [0,1,2,3]);
-    this.walkUp = this.player.animations.add('walkRight', [15,16,17,18]);
-    this.walkUp = this.player.animations.add('attack', [14,13,12,11,10]);
-    this.standing = this.player.animations.add('standing', [8,9]);
+    this.player.anchor.setTo(0.5, 0.5);
+    this.sword.anchor.setTo(0.5, 0.5);
+
+    this.sword.animations.add('walkDown', [5,6,7,8]);
+    this.sword.animations.add('walkUp', [0,1,2,3]);
+    this.sword.animations.add('walkRight', [37,38,39,40]);
+
+    this.sword.animations.add('standingDown', [10,11]);
+    this.sword.animations.add('standingUp', [13,14]);
+    this.sword.animations.add('standingRight', [16,17]);
+
+    this.player.animations.add('walkDown', [5,6,7,8]);
+    this.player.animations.add('walkUp', [0,1,2,3]);
+    this.player.animations.add('walkRight', [37,38,39,40]);
+    
+    this.player.animations.add('standingDown', [10,11]);
+    this.player.animations.add('standingUp', [13,14]);
+    this.player.animations.add('standingRight', [16,17]);
+    // this.walkUp = this.player.animations.add('attack', [14,13,12,11,10]);
+
   },
-
   update: function(){
 
-    checkDirection.call(this);
-    if (this.input.keyboard.isDown(Phaser.Keyboard.A)){
-      this.player.x -= this.playerSpeed;
+    this.movingLeft = this.input.keyboard.isDown(Phaser.Keyboard.A);
+    this.movingRight = this.input.keyboard.isDown(Phaser.Keyboard.D);
+    this.movingDown = this.input.keyboard.isDown(Phaser.Keyboard.S);
+    this.movingUp = this.input.keyboard.isDown(Phaser.Keyboard.W);
+
+    saveDirection.call(this);
+    // animateStanding.call(this);
+    move.call(this);
+    animate.call(this);
+
+    
+    function saveDirection(){
+      if (this.movingLeft){
+        this.movementDirection = 'left';
+      } else if (this.movingRight){
+        this.movementDirection = 'right';
+      } else if (this.movingUp){
+        this.movementDirection = 'up';
+      } else if (this.movingDown){
+        this.movementDirection = 'down';
+      }
     }
-    if (this.input.keyboard.isDown(Phaser.Keyboard.D)){
-      this.player.x += this.playerSpeed;
-    } 
-    if (this.input.keyboard.isDown(Phaser.Keyboard.W)){
-      this.player.y -= this.playerSpeed;
+
+    function animate(){
+      if(checkIfMoving.call(this)){
+        if (this.movingLeft){
+          this.player.animations.play('walkRight', this.walkingFPS, true);
+          this.sword.animations.play('walkRight', this.walkingFPS, true);
+          this.player.scale.setTo(-1,1);
+        } else if (this.movingRight){
+          this.player.animations.play('walkRight', this.walkingFPS, true);
+          this.sword.animations.play('walkRight', this.walkingFPS, true);
+          this.player.scale.setTo(1,1);
+        } else if (this.movingUp){
+          this.player.animations.play('walkUp', this.walkingFPS, true);
+          this.sword.animations.play('walkUp', this.walkingFPS, true);
+        } else if (this.movingDown){
+          this.player.animations.play('walkDown', this.walkingFPS, true);
+          this.sword.animations.play('walkDown', this.walkingFPS, true);
+        }
+      } else {
+        if (this.movementDirection === 'left'){
+          this.player.animations.play('standingRight', this.standingFPS, true);
+          this.sword.animations.play('standingRight', this.standingFPS, true);
+          this.player.scale.setTo(-1,1);
+        } else if (this.movementDirection === 'right'){
+          this.player.animations.play('standingRight', this.standingFPS, true);
+          this.sword.animations.play('standingRight', this.standingFPS, true);
+          this.player.scale.setTo(1,1);
+        } else if(this.movementDirection === 'up'){
+          this.player.animations.play('standingUp', this.standingFPS, true);
+          this.sword.animations.play('standingUp', this.standingFPS, true);
+        } else if(this.movementDirection === 'down'){
+          this.player.animations.play('standingDown', this.standingFPS, true);
+          this.sword.animations.play('standingDown', this.standingFPS, true);
+        }
+      }
     }
-    if (this.input.keyboard.isDown(Phaser.Keyboard.S)){
-      this.player.y += this.playerSpeed;
+
+    function move(){
+      if (this.movingLeft){
+        checkUpDown.call(this);
+        this.player.x -= this.playerSpeed;
+      } else if (this.movingRight){
+        checkUpDown.call(this);
+        this.player.x += this.playerSpeed;
+      } else if (this.movingUp){
+        this.player.y -= this.playerSpeed;
+      } else if (this.movingDown){
+        this.player.y += this.playerSpeed;
+      }
     }
-    if(!checkMovement.call(this)){
-      this.player.animations.play('standing', 1, true);
+
+    function checkUpDown(){
+      if(this.movingDown){
+        this.player.y += this.playerSpeed;
+      } else if(this.movingUp){
+        this.player.y -= this.playerSpeed;
+      }
     }
-    if(this.input.keyboard.event){
-      this.keyPressed = this.input.keyboard.event.which;
-    }
-    function checkMovement(){
+
+    function checkIfMoving(){
       if(this.input.keyboard.isDown(Phaser.Keyboard.A)
         || this.input.keyboard.isDown(Phaser.Keyboard.D)
         || this.input.keyboard.isDown(Phaser.Keyboard.W)
@@ -66,29 +149,6 @@ BasicGame.Level1.prototype = {
         return true;
       } else {
         return false;
-      }
-    }
-
-    function checkDirection(){
-      if(this.input.keyboard.event && this.input.keyboard.event.which !== this.keyPressed){
-        if (this.input.keyboard.isDown(Phaser.Keyboard.A)){
-          this.walkUp = this.player.animations.play('walkRight', 6, true);
-          this.player.scale.setTo(-1,1);
-          this.movementDirection = 'left';
-        }
-        if (this.input.keyboard.isDown(Phaser.Keyboard.D)){
-          this.walkUp = this.player.animations.play('walkRight', 6, true);
-          this.player.scale.setTo(1,1);
-          this.movementDirection = 'right';
-        }
-        if (this.input.keyboard.isDown(Phaser.Keyboard.W)){
-          this.walkUp = this.player.animations.play('walkUp', 6, true);
-          this.movementDirection = 'up';
-        }
-        if (this.input.keyboard.isDown(Phaser.Keyboard.S)){
-          this.player.animations.play('walkDown', 6, true);
-          this.movementDirection = 'down';
-        }
       }
     }
   }
